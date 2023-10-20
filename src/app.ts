eab098cf30c0c3c24f1ca57196e0546504270acb
henrique weiand <henriqueweiand@gmail.com>
2023-10-19 21:36:16 -0300

    add database and updated prisma version

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'henrique weiand',
+        email: 'henriqueweiand@gmail.com',
+    },
+})
