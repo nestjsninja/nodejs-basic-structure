3a4e1a141ac083ebeb823d4d7096225e380f7efd
henrique weiand <henriqueweiand@gmail.com>
2023-10-20 18:14:59 -0300

    fixed e2e

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,27 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import request from 'supertest'
+
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    },
+  })
+
+  const authResponse = await request(app.server).post('/sessions').send({
+    email: 'johndoe@example.com',
+    password: '123456',
+  })
+
+  const { token } = authResponse.body
+
+  return {
+    token,
+  }
+}
