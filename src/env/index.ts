778d280f533cd8dcf82ffe14d9b92edb74997d58
Henrique Weiand <henriqueweiand@gmail.com>
2023-10-21 14:20:48 -0300

    Update index.ts

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
